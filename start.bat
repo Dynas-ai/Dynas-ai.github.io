5a0fe981374c418313cc9e53728d1ac4e5db477d
DynasProgram <romasinalubov175@gmail.com>
2026-06-18 18:41:52 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+title Dynas AI
+echo ==============================
+echo    🧠 DYNAS AI - ЗАПУСК
+echo ==============================
+echo.
+echo 🔥 Сервер запускается...
+echo 📱 Открой браузер: http://localhost:3000
+echo.
+echo ⚠️  Чтобы остановить сервер - закрой это окно
+echo ==============================
+echo.
+node server.js
+pause
